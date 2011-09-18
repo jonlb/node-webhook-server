08f5f25b8e5ad824a999b5438e9b9b2c629c72ff
Jonathan Bomgardner <jonlb@comcast.net>
2011-09-17 20:55:27 -0700

    update to push out

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello world\nI've changed, again!!");
+  res.end("Hello world\nI've changed, again!! And again :) ");
 });
  
 server.listen(8080);
